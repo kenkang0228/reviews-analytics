c5b8077ad43259adaafead6682ec83b21dc5f837
Ken Kang <kenkang0228@gmail.com>
2021-06-20 16:47:21 +0800

    計算留言長度

--- reviews-analytics.py
+++ reviews-analytics.py
@@ -1,11 +1,19 @@
 data = []
-count = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		data.append(line)
-		count += 1
-		if count % 2 == 0:
-			print(len(data))
+		data.append(line)	
+		
 
+print('讀取完成，總共有', len(data), '筆資料')
 
-print(data[1]) 
+sum_len = 0
+for x in data:
+	sum_len = sum_len + len(x)
+
+print('1.', data[0].strip()) #印出檔案中第1筆資料
+print('2.', data[1].strip()) #印出檔案中第2筆資料
+print('3.', data[2].strip()) #印出檔案中第3筆資料
+print('4.', data[3].strip()) #印出檔案中第4筆資料
+print('5.', data[4].strip()) #印出檔案中第5筆資料
+print('留言的平均長度為', sum_len/len(data), '個字元')
